b3001af34925154db06b9afcf0f2b7bb2018c606
pengcheng <pengcheng@wondersgroup.com>
2025-10-20 22:31:44 +0800

    砀山数据归档

--- mes-edge-portal-water/src/main/resources/static/js/menu.js
+++ mes-edge-portal-water/src/main/resources/static/js/menu.js
@@ -17,6 +17,47 @@ var menu = [
                 'name': 'menu.task',
                 'path': '/view/task_tables'
             },
+            {
+                'id': 43,
+                'name': 'menu.datalist',
+                'path': '/view/report_datalist_tables'
+            }
+        ]
+    },
+    {
+        'id': 5,
+        'name': 'menu.central.devrun',
+        'children': [
+            {
+                'id': 31,
+                'name': 'menu.mag',
+                'path': '/view/report_mag_tables'
+            },
+            {
+                'id': 32,
+                'name': 'menu.wps',
+                'path': '/view/report_wps_tables'
+            },
+            {
+                'id': 33,
+                'name': 'menu.dosing',
+                'path': '/view/report_dosing_tables'
+            },
+            {
+                'id': 34,
+                'name': 'menu.od11',
+                'path': '/view/report_od11_tables'
+            },
+            {
+                'id': 35,
+                'name': 'menu.od12',
+                'path': '/view/report_od12_tables'
+            },
+            {
+                'id': 36,
+                'name': 'menu.od20',
+                'path': '/view/report_od20_tables'
+            },
         ]
     }
 ];
