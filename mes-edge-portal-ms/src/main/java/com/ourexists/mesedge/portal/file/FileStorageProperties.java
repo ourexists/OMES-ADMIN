0bbf617646977065fca7be4fa1abdcc4d6197880
pengcheng <434713950@qq.com>
2026-02-27 09:40:22 +0800

    1.设备json配置 2.本地化文件存储管理

---
+++ mes-edge-portal-ms/src/main/java/com/ourexists/mesedge/portal/file/FileStorageProperties.java
@@ -0,0 +1,23 @@
+package com.ourexists.mesedge.portal.file;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "file.storage")
+public class FileStorageProperties {
+
+    /**
+     * 本地存储根目录，可以是绝对路径或相对路径
+     */
+    private String rootPath = "file";
+
+    /**
+     * 对外访问前缀（可选）
+     */
+    private String urlPrefix = "/files";
+}
